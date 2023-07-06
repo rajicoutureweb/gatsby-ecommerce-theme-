cd88f7215019defb6f1b854a94cdf73548e55b38
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-06 21:06:25 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: `RajiCouture`,
+    siteUrl: `https://rajicouture.com`,
   },
   plugins: [
     {
