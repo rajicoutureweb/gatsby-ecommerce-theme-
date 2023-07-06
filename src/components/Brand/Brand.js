dcd5ff2785525d40c81e86d3cd3ce489c5b3a35c
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-06 21:22:16 +0200

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,1 +1,18 @@
-import React from 'react';import { navigate } from 'gatsby';import * as styles from './Brand.module.css';const Brand = (props) => {return (<divclassName={styles.root}role={'presentation'}onClick={() => navigate('/')}><h4>RAJICOUTURE</h4></div>);};export default Brand;
+import React from 'react';
+import { navigate } from 'gatsby';
+
+import * as styles from './Brand.module.css';
+
+const Brand = (props) => {
+  return (
+    <div
+      className={styles.root}
+      role={'presentation'}
+      onClick={() => navigate('/')}
+    >
+     <h4>RAJICOUTURE</h4> 
+    </div>
+  );
+};
+
+export default Brand;
